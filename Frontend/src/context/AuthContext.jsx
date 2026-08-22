ef0a628a99571282f7da1b2f4220761664fe244a
ishan <ishanpatel9785@gmail.com>
2026-08-22 11:46:32 +0530

    made a context api for my authentication

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,12 +1,34 @@
-import {createContext}from 'react'
-
+import axios from 'axios';
+import {createContext,useState ,useEffect}from 'react'
 const AuthContext = createContext();  //yeh el shared channel state create karta hai.
 
 //createing the provider 
 export const AuthProvider=({children})=>{
-  <AuthContext.Provider value={{}}>
+  const [User, setUser] = useState(null);
+  const [loading,setLoading]=useState(true);
+  useEffect(() => {
+  const checkAuth=async()=>{
+    setLoading(true);
+    try {
+      const response=await axios.get("http://localhost:3000/api/auth/me",{withCredentials:true});
+      setUser(response.data.data);
+    } catch (error) {
+      setUser(null);
+      console.log(error)
+    }finally{
+      setLoading(false);
+    }
+  }
+  checkAuth();
+}, [])
+if(loading)return <h1>.....loading.....</h1>
+  
+  return (
+  <AuthContext.Provider value={{User,loading}}>
     {children}
   </AuthContext.Provider>
+
+  )
 }
 
 export default AuthContext
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,17 @@ import { AuthProvider } from './context/AuthContext.jsx'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StrictMode } from 'react'
+
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
+  <StrictMode>
+
     <BrowserRouter>
+  <AuthProvider>
+
     <App />
-  </BrowserRouter>
   </AuthProvider>
+  </BrowserRouter>
+  </StrictMode>
 )
